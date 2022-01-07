a35b48e8bc6c3956d45ab19e0c1759551aedeb4c
Paco Gómez Granados <pacogomezgranados@gmail.com>
2022-01-07 10:57:40 +0100

    Very basic p5 drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useRef } from 'react';
+import P5 from 'p5';
 import './App.css';
 
-function App() {
+// Creating the sketch itself
+const sketch = (p5: P5) => {
+    p5.setup = () => {
+        p5.createCanvas(200, 200);
+    };
+
+    p5.draw = () => {
+        p5.background(220);
+        p5.ellipse(50, 50, 80, 80);
+    };
+};
+
+export default function App() {
+    const sketchRef = useRef(null);
+    useEffect(() => {
+        if (!sketchRef.current) {
+            return () => {};
+        }
+        const instance = new P5(sketch, sketchRef.current);
+        return () => {
+            instance.remove();
+        };
+    }, [sketchRef]);
+
     return (
         <div className="App">
-            <div>Creative coding</div>
+            <div ref={sketchRef}></div>
         </div>
     );
 }
-
-export default App;
