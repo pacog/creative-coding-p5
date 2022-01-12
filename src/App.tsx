6b6f9811331d7de1d12124ec48cd749e8a8bfa95
Paco Gómez Granados <pacogomezgranados@gmail.com>
2022-01-12 15:36:03 +0100

    Coloring when collision

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import React, { useMemo, useState, useEffect, useRef } from 'react';
 import { Line, Point, Bounds } from '@mathigon/euclid';
 import useMeasure from 'react-use-measure';
 import P5 from 'p5';
-import { random } from 'lodash';
+import { now, random } from 'lodash';
+import chroma from 'chroma-js';
 import './App.css';
 
-const BALL_SIZE = 10;
+const BALL_SIZE = 40;
 const TOTAL_BALLS = 1000;
 const MIN_BALL_SPEED = 30;
 const MAX_BALL_SPEED = 100;
 const DISTANCE_TO_MOUSE = 100;
 const MAX_PUNCH_SPEED = 50;
-const SECONDS_TO_DAMPEN = 5;
+const SECONDS_TO_DAMPEN = 3;
+const TIME_WITH_COLOR_AFTER_COLLISION = 3;
+
+const BG_COLOR = '#10002b';
+const COLOR_SCALE = chroma.scale([
+    '#3c096c',
+    '#5a189a',
+    '#7b2cbf',
+    '#9d4edd',
+    '#c77dff',
+]);
 
 function getSketchDefinition(size: { width: number; height: number }) {
     if (!size.width || !size.height) {
@@ -29,12 +40,14 @@ function getSketchDefinition(size: { width: number; height: number }) {
         .map(() => new Circle({ containedIn: container }));
 
     const sketchDefinition = (p5: P5) => {
+        p5.disableFriendlyErrors = true;
+
         p5.setup = () => {
             p5.createCanvas(size.width, size.height);
         };
 
         p5.draw = () => {
-            p5.background(220);
+            p5.background(BG_COLOR);
             circles.forEach((circle) => circle.draw(p5));
             circles.forEach((circle) => circle.update(p5));
         };
@@ -48,6 +61,9 @@ class Circle {
     private defaultSpeed: number;
     private directionRadians: number;
     private containedIn: Bounds;
+    private originalColorInScale: number;
+    private colorInScale: number;
+    private lastCollision: number;
 
     constructor({ containedIn }: { containedIn: Bounds }) {
         this.containedIn = containedIn;
@@ -55,10 +71,16 @@ class Circle {
         this.defaultSpeed = random(MIN_BALL_SPEED, MAX_BALL_SPEED, true);
         this.speedPxPerSecond = this.defaultSpeed;
         this.directionRadians = random(0, 2 * Math.PI, true);
+        this.originalColorInScale = random(0, 0.3);
+        this.colorInScale = this.originalColorInScale;
+        this.lastCollision = 0;
     }
 
     draw(p5: P5) {
-        p5.circle(this.position.x, this.position.y, BALL_SIZE);
+        p5.noStroke();
+        const color = COLOR_SCALE(this.colorInScale).rgb();
+        p5.fill(color[0], color[1], color[2], 200);
+        p5.square(this.position.x, this.position.y, BALL_SIZE);
     }
 
     update(p5: P5) {
@@ -69,6 +91,7 @@ class Circle {
         this.updateDirection(p5);
         this.updatePosition(deltaSeconds);
         this.dampenSpeed(deltaSeconds);
+        this.updateColor();
     }
 
     private updateDirection(p5: P5) {
@@ -80,6 +103,7 @@ class Circle {
                 0,
                 2 * Math.PI
             );
+            this.lastCollision = now();
         } else if (distance < DISTANCE_TO_MOUSE) {
             const newDirection = new Line(mouseCurrent, this.position);
             this.directionRadians = newDirection.angle;
@@ -89,6 +113,7 @@ class Circle {
                 MAX_PUNCH_SPEED
             );
             this.speedPxPerSecond += moved;
+            this.lastCollision = now();
         }
     }
 
@@ -123,6 +148,26 @@ class Circle {
             Point.sum(this.position, delta),
             this.containedIn
         );
+    }
+
+    private updateColor() {
+        const timeSinceLastCollision = now() - this.lastCollision;
+
+        if (timeSinceLastCollision < TIME_WITH_COLOR_AFTER_COLLISION * 1000) {
+            const factor =
+                1 -
+                timeSinceLastCollision /
+                    (TIME_WITH_COLOR_AFTER_COLLISION * 1000);
+            this.colorInScale = project(
+                factor,
+                0,
+                1,
+                this.originalColorInScale,
+                1
+            );
+        } else {
+            this.colorInScale = this.originalColorInScale;
+        }
     }
 }
 
@@ -186,4 +231,15 @@ function keepNumberInside(n: number, lowerLimit: number, upperLimit: number) {
         res -= size;
     }
     return res;
+}
+
+function project(
+    n: number,
+    nMin: number,
+    nMax: number,
+    destMin: number,
+    destMax: number
+) {
+    const percentageInOrigin = n / (nMax - nMin);
+    return destMin + percentageInOrigin * (destMax - destMin);
 }
