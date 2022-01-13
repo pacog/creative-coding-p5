1f2e0f42564740f16376ccf9370c4f89279689e1
Paco Gómez Granados <pacogomezgranados@gmail.com>
2022-01-13 17:48:31 +0100

    3 circles recursive

--- pages/pieces/2-random-fractals.tsx
+++ pages/pieces/2-random-fractals.tsx
@@ -1,7 +1,8 @@
-import { Bounds, Point } from '@mathigon/euclid';
+import { Bounds, Circle, Point } from '@mathigon/euclid';
 import P5Sketch from 'components/P5Sketch';
 import type P5 from 'p5';
 import chroma from 'chroma-js';
+import { range } from 'lodash';
 
 export default function RandomFractals() {
     return <P5Sketch getSketchDefinition={getSketchDefinition} />;
@@ -17,6 +18,7 @@ function getSketchDefinition(size: { width: number; height: number }) {
 
         p5.setup = () => {
             p5.createCanvas(size.width, size.height);
+            p5.noLoop();
         };
 
         p5.draw = () => {
@@ -39,6 +41,8 @@ function getColorForDepth(depth: number) {
     }
     return '#000';
 }
+// https://math.stackexchange.com/questions/919445/diameter-of-a-circle-touching-three-inner-circles-of-diameter-1
+const constantSmallerCirclesRadius = Math.sqrt(3) / (2 + Math.sqrt(3));
 
 function drawRecursiveCircle(
     p5: P5,
@@ -46,23 +50,24 @@ function drawRecursiveCircle(
     diameter: number,
     depth: number
 ) {
-    if (depth >= 7) {
+    if (depth >= 8) {
         return;
     }
     p5.noStroke();
     const color = chroma(getColorForDepth(depth)).rgb();
     p5.fill(color[0], color[1], color[2]);
     p5.circle(center.x, center.y, diameter);
-    drawRecursiveCircle(
-        p5,
-        new Point(center.x, center.y - diameter / 4),
-        diameter / 2,
-        depth + 1
-    );
-    drawRecursiveCircle(
-        p5,
-        new Point(center.x, center.y + diameter / 4),
-        diameter / 2,
-        depth + 1
-    );
+
+    const circlesToPaint = 3;
+    const smallCircleRadius = (diameter / 2) * constantSmallerCirclesRadius;
+    const circle = new Circle(center, diameter / 2 - smallCircleRadius);
+
+    range(circlesToPaint).forEach((index) => {
+        drawRecursiveCircle(
+            p5,
+            circle.at(index / circlesToPaint),
+            smallCircleRadius * 2,
+            depth + 1
+        );
+    });
 }
