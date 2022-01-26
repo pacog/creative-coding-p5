aaddda00228e4b703e456ba033c6a2f9712335dd
Paco Gómez Granados <pacogomezgranados@gmail.com>
2022-01-26 14:01:46 +0100

    Squares for spirals

--- pages/pieces/3-spiraling.tsx
+++ pages/pieces/3-spiraling.tsx
@@ -1,9 +1,9 @@
-import { Bounds, Point } from '@mathigon/euclid';
+import { Bounds, Point, Rectangle } from '@mathigon/euclid';
 import type P5 from 'p5';
 import chroma from 'chroma-js';
 import P5Sketch from 'components/P5Sketch';
 import PieceLayout from 'components/PieceLayout';
-import { random } from 'lodash';
+import { random, update } from 'lodash';
 
 export default function RandomFractals() {
     return (
@@ -13,9 +13,9 @@ export default function RandomFractals() {
     );
 }
 
-const SPIRALS = 1;
+const SPIRALS = 100;
 const MIN_START = 1;
-const MAX_START = 1;
+const MAX_START = 20;
 
 function getSketchDefinition(size: { width: number; height: number }) {
     if (!size.width || !size.height) {
@@ -45,56 +45,95 @@ function getSketchDefinition(size: { width: number; height: number }) {
 }
 
 function drawSpiral(p5: P5, fibonacciStart = 1, startingDirectionIndex = 0) {
-    const initialPoint = new Point(p5.width / 2, p5.height / 2);
-    const color = chroma.random().rgb();
-    p5.stroke(...color);
-
-    const spiral = spiralPointsGenerator(
+    const initialPoint = new Point(
+        Math.floor(p5.width / 3),
+        Math.floor(p5.height / 3)
+    );
+    const squares = spiralSquaresGenerator(
         initialPoint,
         new Bounds(0, p5.width, 0, p5.height),
         fibonacciStart,
         startingDirectionIndex
     );
-    let prevPoint: Point;
-    for (const point of spiral) {
-        if (prevPoint) {
-            p5.line(prevPoint.x, prevPoint.y, point.x, point.y);
-        }
-        prevPoint = point;
+    const color = chroma.random().rgb();
+    p5.stroke(...color);
+    p5.noFill();
+
+    for (const square of squares) {
+        p5.rect(square.p.x, square.p.y, square.w, square.h);
     }
 }
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
-function* spiralPointsGenerator(
+function* spiralSquaresGenerator(
     initialPoint: Point,
     bounds: Bounds,
     fibonacciStart = 1,
     startingDirectionIndex = 0
 ) {
     const directions = [
-        new Point(1, 0),
-        new Point(0, 1),
-        new Point(-1, 0),
-        new Point(0, -1),
+        Direction.Right,
+        Direction.Up,
+        Direction.Left,
+        Direction.Down,
     ];
     let directionIndex = startingDirectionIndex;
-    yield initialPoint;
-    let nextPoint = initialPoint;
     const fibonacci = fibonacciGenerator(fibonacciStart);
-    while (bounds.contains(nextPoint)) {
-        const direction = directions[directionIndex];
-        const fibValue = fibonacci.next().value || 0;
-        nextPoint = nextPoint.add(
-            new Point(direction.x * fibValue, direction.y * fibValue)
+    let fibValue = fibonacci.next().value || 0;
+    let lastRectangle = new Rectangle(initialPoint, fibValue, fibValue);
+    yield lastRectangle;
+
+    while (lastRectangle.w < bounds.dx || lastRectangle.h < bounds.dy) {
+        fibValue = fibonacci.next().value || 0;
+        lastRectangle = getRectangleFromLastAndDirection(
+            lastRectangle,
+            directions[directionIndex],
+            fibValue
         );
-        yield nextPoint;
+        yield lastRectangle;
         directionIndex = (directionIndex + 1) % directions.length;
     }
 }
 
+function getRectangleFromLastAndDirection(
+    rectangle: Rectangle,
+    direction: Direction,
+    size: number
+) {
+    let startingPoint: Point;
+    switch (direction) {
+        case Direction.Right:
+            startingPoint = new Point(
+                rectangle.p.x + rectangle.w,
+                rectangle.p.y + rectangle.h - size
+            );
+            break;
+        case Direction.Up:
+            startingPoint = new Point(
+                rectangle.p.x + rectangle.w - size,
+                rectangle.p.y - size
+            );
+            break;
+        case Direction.Left:
+            startingPoint = new Point(rectangle.p.x - size, rectangle.p.y);
+            break;
+        case Direction.Down:
+            startingPoint = new Point(
+                rectangle.p.x,
+                rectangle.p.y + rectangle.h
+            );
+            break;
+    }
+    return new Rectangle(startingPoint, size, size);
+}
 function* fibonacciGenerator(fibonacciStart = 1) {
     let prev1 = fibonacciStart;
     let prev2 = fibonacciStart;
-    yield prev1;
     yield prev2;
     while (true) {
         let newValue = prev1 + prev2;
