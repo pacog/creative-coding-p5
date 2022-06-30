10f95f53b234cfbe9265a0a6e3d148718b7d47ac
Paco Gómez Granados <pacogomezgranados@gmail.com>
2022-06-30 21:30:56 +0200

    Show images in layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Image from 'next/image';
+import { sortBy } from 'lodash';
 import { pieces } from 'utils/Pieces';
 import styles from './index.module.css';
-import { sortBy } from 'lodash';
 
 const piecesArray = Object.entries(pieces).map(([id, entry]) => ({
     ...entry,
@@ -29,7 +30,21 @@ export default function Index() {
                             <li key={`${piece.id}__${piece.url}`}>
                                 <Link href={piece.url}>
                                     <a className={styles.piece}>
-                                        {piece.id}. {piece.title}
+                                        <div className={styles.pieceBG}>
+                                            <Image
+                                                layout="fill"
+                                                src={piece.previewImg}
+                                                alt=""
+                                            />
+                                        </div>
+
+                                        <div className={styles.pieceNumber}>
+                                            {piece.id}
+                                        </div>
+
+                                        <div className={styles.pieceTitle}>
+                                            {piece.title}
+                                        </div>
                                     </a>
                                 </Link>
                             </li>
--- utils/Pieces.ts
+++ utils/Pieces.ts
@@ -1,42 +1,59 @@
+import preview1 from './previewPieces/1.png';
+import preview2 from './previewPieces/2.png';
+import preview3 from './previewPieces/3.png';
+import preview4 from './previewPieces/4.png';
+import preview5 from './previewPieces/5.png';
+import preview6 from './previewPieces/6.png';
+import preview7 from './previewPieces/7.png';
+import preview8 from './previewPieces/8.png';
+
 export const pieces = {
     1: {
         title: 'A bit squared',
         description: 'Squares floating around and being pushed by your mouse',
         url: '/pieces/1-a-bit-squared',
+        previewImg: preview1,
     },
     2: {
         title: 'Random fractals',
         description: 'Fun with fractal circles',
         url: '/pieces/2-random-fractals',
+        previewImg: preview2,
     },
     3: {
         title: 'Spiraling',
         description: 'Fibonacci spirals',
         url: '/pieces/3-spiraling',
+        previewImg: preview3,
     },
     4: {
         title: 'Aproxigraphy',
         description: 'Aproximating a photo with shapes',
         url: '/pieces/4-aproxigraphy',
+        previewImg: preview4,
     },
     5: {
         title: 'Lines? Lines!',
         description: 'Lines auto creating art',
         url: '/pieces/5-lines-lines',
+        previewImg: preview5,
     },
     6: {
         title: 'Game of life',
         description: "Yet another Conway's game of life",
         url: '/pieces/6-game-of-life',
+        previewImg: preview6,
     },
     7: {
         title: 'Spirofan',
         description: 'Spirograph inspired',
         url: '/pieces/7-spirofan',
+        previewImg: preview7,
     },
     8: {
         title: 'Blanket',
         description: '3d blanket',
         url: '/pieces/8-blanket',
+        previewImg: preview8,
     },
 };
