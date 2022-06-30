a36f2a6e2baacffa20d59a4cbd9466db70c2ac35
Paco Gómez Granados <pacogomezgranados@gmail.com>
2022-06-30 21:05:46 +0200

    Grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,14 @@ import Link from 'next/link';
 import Head from 'next/head';
 import { pieces } from 'utils/Pieces';
 import styles from './index.module.css';
+import { sortBy } from 'lodash';
+
+const piecesArray = Object.entries(pieces).map(([id, entry]) => ({
+    ...entry,
+    id,
+}));
+
+const orderedPieces = sortBy(piecesArray, (piece) => -parseInt(piece.id, 10));
 
 export default function Index() {
     return (
@@ -16,47 +24,16 @@ export default function Index() {
                         <span>Coding</span>
                         <span>p5js</span>
                     </h1>
-                    <ul>
-                        <li>
-                            <Link href="/pieces/1-a-bit-squared">
-                                <a>1. {pieces[1].title}</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/pieces/2-random-fractals">
-                                <a>2. {pieces[2].title}</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/pieces/3-spiraling">
-                                <a>3. {pieces[3].title}</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/pieces/4-aproxigraphy">
-                                <a>4. {pieces[4].title}</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/pieces/5-lines-lines">
-                                <a>5. {pieces[5].title}</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/pieces/6-game-of-life">
-                                <a>6. {pieces[6].title}</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/pieces/7-spirofan">
-                                <a>7. {pieces[7].title}</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/pieces/8-blanket">
-                                <a>8. {pieces[8].title}</a>
-                            </Link>
-                        </li>
+                    <ul className={styles.piecesList}>
+                        {orderedPieces.map((piece) => (
+                            <li key={`${piece.id}__${piece.url}`}>
+                                <Link href={piece.url}>
+                                    <a className={styles.piece}>
+                                        {piece.id}. {piece.title}
+                                    </a>
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
--- utils/Pieces.ts
+++ utils/Pieces.ts
@@ -2,33 +2,41 @@ export const pieces = {
     1: {
         title: 'A bit squared',
         description: 'Squares floating around and being pushed by your mouse',
+        url: '/pieces/1-a-bit-squared',
     },
     2: {
         title: 'Random fractals',
         description: 'Fun with fractal circles',
+        url: '/pieces/2-random-fractals',
     },
     3: {
         title: 'Spiraling',
         description: 'Fibonacci spirals',
+        url: '/pieces/3-spiraling',
     },
     4: {
         title: 'Aproxigraphy',
         description: 'Aproximating a photo with shapes',
+        url: '/pieces/4-aproxigraphy',
     },
     5: {
         title: 'Lines? Lines!',
         description: 'Lines auto creating art',
+        url: '/pieces/5-lines-lines',
     },
     6: {
         title: 'Game of life',
         description: "Yet another Conway's game of life",
+        url: '/pieces/6-game-of-life',
     },
     7: {
         title: 'Spirofan',
         description: 'Spirograph inspired',
+        url: '/pieces/7-spirofan',
     },
     8: {
         title: 'Blanket',
         description: '3d blanket',
+        url: '/pieces/8-blanket',
     },
 };
