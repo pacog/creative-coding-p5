c317697a2a9b2679030ca8e5d68ad34bc3ea7631
Paco Gómez Granados <pacogomezgranados@gmail.com>
2022-06-30 09:12:49 +0200

    Start styling

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,19 @@ export default class MyDocument extends Document {
                     />
                     <meta name="msapplication-TileColor" content="#da532c" />
                     <meta name="theme-color" content="#ffffff" />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin={true}
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Roboto&display=swap"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <body>
                     <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,64 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import { pieces } from 'utils/Pieces';
+import styles from './index.module.css';
 
 export default function Index() {
     return (
         <>
             <Head>
-                <title>Creative coding with P5</title>
+                <title>Creative coding with P5 - pacog</title>
             </Head>
-            <div>
-                <h1>Creative coding p5</h1>
-                <ul>
-                    <li>
-                        <Link href="/pieces/1-a-bit-squared">
-                            <a>1. {pieces[1].title}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/pieces/2-random-fractals">
-                            <a>2. {pieces[2].title}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/pieces/3-spiraling">
-                            <a>3. {pieces[3].title}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/pieces/4-aproxigraphy">
-                            <a>4. {pieces[4].title}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/pieces/5-lines-lines">
-                            <a>5. {pieces[5].title}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/pieces/6-game-of-life">
-                            <a>6. {pieces[6].title}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/pieces/7-spirofan">
-                            <a>7. {pieces[7].title}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/pieces/8-blanket">
-                            <a>8. {pieces[8].title}</a>
-                        </Link>
-                    </li>
-                </ul>
+            <div className={styles.root}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>
+                        <span>Creative</span>
+                        <span>Coding</span>
+                        <span>p5js</span>
+                    </h1>
+                    <ul>
+                        <li>
+                            <Link href="/pieces/1-a-bit-squared">
+                                <a>1. {pieces[1].title}</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/pieces/2-random-fractals">
+                                <a>2. {pieces[2].title}</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/pieces/3-spiraling">
+                                <a>3. {pieces[3].title}</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/pieces/4-aproxigraphy">
+                                <a>4. {pieces[4].title}</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/pieces/5-lines-lines">
+                                <a>5. {pieces[5].title}</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/pieces/6-game-of-life">
+                                <a>6. {pieces[6].title}</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/pieces/7-spirofan">
+                                <a>7. {pieces[7].title}</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/pieces/8-blanket">
+                                <a>8. {pieces[8].title}</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </>
     );
