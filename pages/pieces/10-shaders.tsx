a0a14b634957732743c09b697dd921bd015d1097
Paco Gómez Granados <pacogomezgranados@gmail.com>
2024-03-06 16:19:14 +0100

    More effects

--- pages/pieces/10-shaders.tsx
+++ pages/pieces/10-shaders.tsx
@@ -59,17 +59,39 @@ uniform float time;
 uniform float repetitions;
 const float pi = 3.1415926535897932384626433832795;
 
+vec3 rgb2hsb(vec3 c){
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = c.g < c.b ? vec4(c.bg, K.wz) : vec4(c.gb, K.xy);
+    vec4 q = c.r < p.x ? vec4(p.xyw, c.r) : vec4(c.r, p.yzx);
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsb2rgb(vec3 c){
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main() {
 
     vec2 uv = vTexCoord;
     uv = 1.0 - uv;
     vec2 mirrorUvs = abs(uv * repetitions - (repetitions / 2.0));
     vec2 sinUvs = mirrorUvs;
-    sinUvs.x = time + cos(mirrorUvs.x * pi) + sin(mirrorUvs.x * pi);
-    sinUvs.y = time + cos(mirrorUvs.y * pi) + sin((mirrorUvs.x) * pi);
+    sinUvs.x = time + cos((time + mirrorUvs.x) * pi) + sin(mirrorUvs.x * pi);
+    sinUvs.y = time + cos(time - mirrorUvs.y * pi) + sin((mirrorUvs.x) * pi);
     vec4 tex = texture2D(inputImage, mod(sinUvs, 1.0));
 
-    gl_FragColor = tex;
+
+    vec3 hsb = rgb2hsb(tex.rgb);
+    hsb.r += time;
+    hsb.r = fract(hsb.r);
+    vec3 rgb = hsb2rgb(hsb);
+
+    gl_FragColor = vec4(rgb, 1.0);
 }
 `;
 
