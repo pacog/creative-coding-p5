21a8fdd2ac396a681d7d8a50cc8a9888dc8e6145
Paco Gómez Granados <pacogomezgranados@gmail.com>
2024-03-06 16:12:54 +0100

    Cleanup code

--- pages/pieces/10-shaders.tsx
+++ pages/pieces/10-shaders.tsx
@@ -62,37 +62,14 @@ const float pi = 3.1415926535897932384626433832795;
 void main() {
 
     vec2 uv = vTexCoord;
-    // the texture is loaded upside down and backwards by default so lets flip it
     uv = 1.0 - uv;
-
-    // this line will make our uvs mirrored
-    // it will convert it into a number that goes 0 to 1 to 0
-    // abs() will turn our negative numbers positive
     vec2 mirrorUvs = abs(uv * repetitions - (repetitions / 2.0));
     vec2 sinUvs = mirrorUvs;
     sinUvs.x = time + cos(mirrorUvs.x * pi) + sin(mirrorUvs.x * pi);
     sinUvs.y = time + cos(mirrorUvs.y * pi) + sin((mirrorUvs.x) * pi);
     vec4 tex = texture2D(inputImage, mod(sinUvs, 1.0));
 
-
-    // output to screen
     gl_FragColor = tex;
-
-    // vec2 p = -1.0 + 2.0 * gl_FragCoord.xy / resolution.xy;
-    // vec2 uv;
-    // float a = 1.0;
-    float a = atan(uv.y, uv.x);
-    // float r = sqrt(dot(p,p));
-
-    // uv.x =          7.0*a/3.1416;
-    // uv.y = -time+ sin(7.0*r+time) + .7*cos(time+7.0*a);
-
-    // float w = .5+.5*(sin(time+7.0*r)+ .7*cos(time+7.0*a));
-
-    // vec3 col =  texture2D(tex0,uv*.5).xyz;
-
-    // gl_FragColor = vec4(col*w,1.0);
-
 }
 `;
 
@@ -114,13 +91,13 @@ const getSketchDefinition = (params: ISketchParams) => {
     return (p5: P5) => {
         // p5.disableFriendlyErrors = true;
         let myShader: P5.Shader;
-        let camera: P5.Element;
+        let camera: P5.MediaElement;
 
         function createCamera() {
             if (camera) {
                 camera.remove();
             }
-            camera = p5.createCapture('video');
+            camera = p5.createCapture('video') as P5.MediaElement;
             camera.size(p5.windowWidth, p5.windowHeight);
             camera.hide();
         }
@@ -140,7 +117,7 @@ const getSketchDefinition = (params: ISketchParams) => {
         };
 
         p5.draw = () => {
-            myShader.setUniform('inputImage', camera as P5.MediaElement);
+            myShader.setUniform('inputImage', camera);
             myShader.setUniform('time', p5.millis() / (params.speed * 1000));
             myShader.setUniform('repetitions', params.repetitions);
             p5.shader(myShader);
